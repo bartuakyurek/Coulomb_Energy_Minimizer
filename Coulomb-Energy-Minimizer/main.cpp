3a41e5bc50265fb9b273cfa23f7eeca3e8fed055
bartuakyurek <bartuakyurek@outlook.com>
2023-05-15 17:13:21 +0300

    Initial Commit

---
+++ Coulomb-Energy-Minimizer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Coulomb-Energy-Minimizer
+//
+//  Created by Bartu on 15.05.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
